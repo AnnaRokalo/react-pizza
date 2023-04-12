fdc57987e860bcdfc4b1c47ecb7f40bdf222b6e2
annarokalo <rokalo.a.s@gmail.com>
2023-04-12 13:17:23 +0300

    Add typescript, styled-components, create first component.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <p>
+         Hello! Main part will be here soon.
+        </p>
+    </div>
+  )
+}
+
+export default App
