98a0e68b6a6dda8d79bc572af58fdf54d2d1449a
annarokalo <rokalo.a.s@gmail.com>
2023-05-04 14:15:58 +0300

    Fix issues after eslint check

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Fragment } from 'react';
 import Header from './components/Header/Header';
 import GlobalStyle from './theme/globalStyles';
 
 function App() {
   return (
-    <>
+    <div>
       <GlobalStyle />
       <div>
         <Header />
-        <p>Hello! Main part will be here soon.</p>
+        <p>Hello! Main part will be here soon. </p>
       </div>
-    </>
+    </div>
   );
 }
 
